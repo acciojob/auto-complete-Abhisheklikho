adfd3805d820f967e9879917140f92a5e408e6f4
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-06-24 15:50:23 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,50 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+
+import React, { useState, useEffect } from "react";
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const updateSuggestions = (value) => {
+      const filteredSuggestions = fruits.filter((fruit) =>
+        fruit.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    };
+
+    const delayDebounceFn = setTimeout(() => {
+      updateSuggestions(searchTerm);
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Search for fruits..."
+      />
+      <ul>
+        {suggestions.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
